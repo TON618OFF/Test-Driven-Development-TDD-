52799dbbac51dba76e307befa805109c01b1f6c7
TON618OFF <rusblackgod@gmail.com>
2024-09-29 19:45:59 +0300

    Добавьте файлы проекта.

---
+++ TDD.AssertStringAssert.Tests/StringCheckTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Globalization;
+
+
+namespace TDD.AssertStringAssert.Tests
+{
+    [TestClass]
+    public class StringCheckTests
+    {
+
+        [TestMethod]
+        public void CheckShortestWord_IfHaveShortestWord_CorrectReturned()
+        {
+            // arrange
+            string input = "Сегодня я вышел на прогулку, ведь это очень полезно!";
+            // act
+            // assert
+            StringAssert.Contains("я", StringCheck.CheckShortestWord(input));
+        }
+
+        [TestMethod]
+        public void CheckCountWords_IfHaveSomethingWord_CorrectReturned()
+        {
+            // arrange
+            string input = "Сегодня я вышел на прогулку, ведь это очень полезно!";
+            // act
+            // assert
+            Assert.AreEqual(9, StringCheck.CheckCountWords(input));
+        }
+
+        [TestMethod]
+        public void CheckCountChars_FindCountCharsInString_CorrectReturned()
+        {
+            // arrange
+            string input = "Сегодня я вышел на прогулку, ведь это очень полезно!";
+            // act
+            // assert
+            Assert.AreEqual(52, StringCheck.CheckCountChars(input));
+        }
+
+        [TestMethod]
+        public void CheckNineChar_FindNineCharInAString_CorrectReturned()
+        {
+            // arrange
+            string input = "Сегодня я вышел на прогулку, ведь это очень полезно!";
+            // act
+            // assert
+            Assert.AreEqual('я', StringCheck.CheckNineChar(input));
+        }
+
+        [TestMethod]
+        public void CheckCountDigits_CheckCountDigitsInAString_CorrectReturned()
+        {
+            // arrange
+            string input = "Сегодня я вышел на прогулку, ведь это очень полезно! 1174";
+            // act
+            // assert
+            Assert.AreEqual(4, StringCheck.CheckCountDigits(input));
+        }
+
+        [TestMethod]
+        public void CheckMaxDigitsInARow_MaxDigitsInAString_CorrectReturned()
+        {
+            // arrange
+            string input = "Сегодня я вышел на прогулку, ведь это очень полезно! 1174";
+            // act
+            // assert
+            Assert.AreEqual(4, StringCheck.CheckMaxDigitsInARow(input));
+        }
+
+        [TestMethod]
+        public void CheckCountAMCombinations_CountAMInAString_CorrectReturned()
+        {
+            // arrange
+            string input = "Сегодня я вышел на прогулку, ведь это очень полезно! 1174 АМ АМ АМ";
+            // act
+            // assert
+            Assert.AreEqual(3, StringCheck.CheckCountAMCombinations(input));
+        }
+    }
+}
---
+++ TDD.AssertStringAssert/StringCheck.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.AssertStringAssert
+{
+    public class StringCheck
+    {
+        public static string CheckShortestWord(string input)
+        {
+            return input.Split(' ').OrderBy(word => word.Length).First();
+        }
+
+        public static int CheckCountWords(string input)
+        {
+            return input.Split(' ').Length;
+        }
+
+        public static int CheckCountChars(string input)
+        {
+            return input.Length;
+        }
+
+        public static char CheckNineChar(string input)
+        {
+            return input[8];
+        }
+
+        public static int CheckCountDigits(string input)
+        {
+            return input.Count(char.IsDigit);
+        }
+
+        public static int CheckMaxDigitsInARow(string input)
+        {
+            int maxCount = 0, currentCount = 0;
+            foreach (char c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    currentCount++;
+                    if (currentCount > maxCount)
+                        maxCount = currentCount;
+                }
+                else
+                {
+                    currentCount = 0;
+                }
+            }
+            return maxCount;
+        }
+
+        public static int CheckCountAMCombinations(string input)
+        {
+            return (input.Length - input.Replace("АМ", "").Length) / 2;
+        }
+    }
+}
